e1f469548a0bd745c9b49e684dd9059eb8a3591a
Jakespitteler <125878678+Jakespitteler@users.noreply.github.com>
2025-05-04 10:52:16 +0800

    step back

--- app/models.py
+++ app/models.py
@@ -12,11 +12,9 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     author = db.Column(db.String(150), nullable=False)
-    genre = db.Column(db.String(100), nullable=True)
     date_read = db.Column(db.Date, nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    reviews = db.relationship('Review', backref='book', lazy=True)
 
 #More models that we could add
 #class Review(db.Model):
