24574d38757e70477125558bbd15a8550c2c3292
AdJud1cator <aj0112471@gmail.com>
2025-05-06 23:25:03 +0800

    Initialised and created the first basic model with User and Book tables having columns

--- app/models.py
+++ app/models.py
@@ -4,16 +4,17 @@ from flask_login import UserMixin
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=True)
     password = db.Column(db.String(128), nullable=False)
-    #books = db.relationship('Book', backref='owner', lazy=True)
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     author = db.Column(db.String(150), nullable=False)
-    genre = db.Column(db.String(150), nullable=True)
-    description = db.Column(db.String(150), nullable=True)
+    genre = db.Column(db.String(100), nullable=True)
+    description = db.Column(db.Text, nullable=True)
 
-#More models that we could add
-#class Review(db.Model):
-#class Tag(db.Model):
+class UserBook(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+class BookShare(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
