8720affaad91a026cc1df370036130f9db25a364
Jakespitteler <125878678+Jakespitteler@users.noreply.github.com>
2025-05-10 18:03:39 +0800

    Blueprints intergrated

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 
+bp = Blueprint('main', __name__)
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()   
@@ -25,6 +26,7 @@ def create_app():
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    from . import routes
+    from app.routes import bp as main_bp
+    app.register_blueprint(main_bp)
 
     return app
