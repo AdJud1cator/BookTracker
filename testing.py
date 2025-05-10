03fdb8f050726651b9b59c4b0f13239528e8b2c2
Jakespitteler <125878678+Jakespitteler@users.noreply.github.com>
2025-05-10 17:07:20 +0800

    Accidentaly left this is file in

--- testing.py
+++
@@ -1,6 +0,0 @@
-from app import db
-from app.models import Book
-new_book = Book(title="Test Book", author="Someone", rating=4.5)
-db.session.add(new_book)
-db.session.commit()
-Book.query.filter_by(title="Test Book").first().rating
