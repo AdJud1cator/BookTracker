cfd506193ed9989f6a2060ce407cda6e1179ef18
Jakespitteler <125878678+Jakespitteler@users.noreply.github.com>
2025-05-16 22:34:37 +0800

    Rendering fixes

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
+from flask_migrate import Migrate
 from .errors import register_error_handlers
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 csrf = CSRFProtect()
+migrate = Migrate()
 
 def create_app(config):
     app = Flask(__name__)
@@ -16,6 +18,7 @@ def create_app(config):
     app.register_blueprint(bp)
 
     db.init_app(app)
+    migrate = Migrate(app, db)
     login_manager.init_app(app)
     login_manager.login_view = 'main.login'
 
--- run.py
+++ run.py
@@ -1,8 +1,6 @@
-from flask_migrate import Migrate
 from app import create_app, db
 from config import DeploymentConfig
 
 if __name__ == '__main__':
     app = create_app(DeploymentConfig)
-    migrate = Migrate(app, db)
     app.run(debug=True)
